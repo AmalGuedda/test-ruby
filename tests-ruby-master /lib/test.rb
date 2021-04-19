c5cea5c4419b645c471954bff01db82ec7c97ac4
AmalGuedda <82024475+AmalGuedda@users.noreply.github.com>
2021-04-19 22:34:25 +0200

    Add files via upload

---
+++ tests-ruby-master /lib/test.rb
@@ -0,0 +1,6 @@
+def température_celius 
+    puts " donner mois la temprérature en celius? "
+    celsius_valeur = gets.chomp 
+    puts (celsius_valeur × 9/5) + 32 = x   ( celsius_valeur = temp en °C) et ( x = temp en °F )
+    return celsius_valeur
+end
